5c6f906b150ae31c87c0ac065e2d8f5d242f1955
Achin Kumar <achin.kumar24@gmail.com>
2016-03-09 15:08:45 +0530

    SlideShowHolder added to hold cell prototype 3

---
+++ app/src/main/java/com/experiment/dashboardmock/controllers/holders/SlideShowHolder.java
@@ -0,0 +1,67 @@
+package com.experiment.dashboardmock.controllers.holders;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.experiment.dashboardmock.R;
+import com.experiment.dashboardmock.model.Element;
+import com.experiment.dashboardmock.model.Item;
+
+/**
+ * Created by Infernus on 08/03/16.
+ */
+public class SlideShowHolder extends BaseHolder {
+    private SliderLayout slider;
+
+    public SlideShowHolder(View itemView) {
+        super(itemView);
+        slider = (SliderLayout) itemView.findViewById(R.id.slide_show_view);
+    }
+
+    @Override
+    public void setupForElement(final Element element) {
+        // Clear and fill SliderLayout with new slides created from items
+        // in the element.
+        slider.removeAllSliders();
+        for(final Item item : element.getItems()) {
+            slider.addSlider(new DefaultSliderView(slider.getContext()).image(item.getImageUrl()).setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+                @Override
+                public void onSliderClick(BaseSliderView slider) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(item.getWebUrl()));
+                    slider.getContext().startActivity(browserIntent);
+                }
+            }));
+        }
+
+        slider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        slider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        slider.setCustomAnimation(new DescriptionAnimation());
+        slider.setDuration(4000);
+
+        // Restore page position using stateInfo.
+        slider.setCurrentPosition(element.getStateInfo());
+
+        // Record Page position as stateInfo whenever page changes.
+        slider.addOnPageChangeListener(new ViewPagerEx.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                element.setStateInfo(position);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+}
