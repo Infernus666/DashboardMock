8adf73aa26ac582289a9a1971f61068677f2260f
Achin Kumar <achin.kumar24@gmail.com>
2016-03-09 15:35:48 +0530

    Setting height of FullWidthImage banner dynamically to 1/2 of screen width

--- app/src/main/java/com/experiment/dashboardmock/controllers/holders/FullWidthImageHolder.java
+++ app/src/main/java/com/experiment/dashboardmock/controllers/holders/FullWidthImageHolder.java
@@ -3,10 +3,12 @@ package com.experiment.dashboardmock.controllers.holders;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.experiment.dashboardmock.R;
 import com.experiment.dashboardmock.model.Element;
+import com.experiment.dashboardmock.utils.DimensionStore;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -19,6 +21,13 @@ public class FullWidthImageHolder extends BaseHolder {
     public FullWidthImageHolder(View itemView) {
         super(itemView);
         imageView = (ImageView) itemView.findViewById(R.id.full_width_image);
+
+        // Setting height of banner image view to be half of screen width
+        // This is assuming that this image is going to be of 2:1 aspect
+        // ratio.
+        ViewGroup.LayoutParams params = imageView.getLayoutParams();
+        params.height = (int) ((float) DimensionStore.getScreenDimension().getWidth() / 2.0f);
+        imageView.setLayoutParams(params);
     }
 
     @Override
