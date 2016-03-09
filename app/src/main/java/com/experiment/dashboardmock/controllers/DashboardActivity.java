2e97279e7476a3ff86fb7194f103be371d9a1fe8
Achin Kumar <achin.kumar24@gmail.com>
2016-03-09 12:42:42 +0530

    Initial commit with Dashboard Activity

---
+++ app/src/main/java/com/experiment/dashboardmock/controllers/DashboardActivity.java
@@ -0,0 +1,20 @@
+package com.experiment.dashboardmock.controllers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.experiment.dashboardmock.R;
+
+/**
+ * Created by Infernus on 07/03/16.
+ */
+public class DashboardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_layout);
+
+    }
+
+}
